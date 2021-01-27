cdbe1dd1657e931bf352c15858220b139bd5160b
Bjunk <pablo.aravenam@gmail.com>
2021-01-27 15:04:15 -0300

    webpay

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -8,11 +8,50 @@
 // Controlador que maneja las interacciones de la página central
 //
 
+require APP_PATH . '/library/init.php';
+
+use Transbank\Webpay\Configuration;
+use Transbank\Webpay\Webpay;
+
 class CartController extends ControllerBase
 {
 
     public function indexAction()
     {
+
+    	$transaction = (new Webpay(Configuration::forTestingWebpayPlusNormal()))
+               ->getNormalTransaction();
+
+        // Monto Transacción 
+        // Si es USD puede llevar 2 decimales
+        // En CLP no llega decimales
+        $amount = 15400;
+
+        // Identificador que será retornado en el callback de resultado:
+        // Creamos SessionID de 16 caracteres
+        $sessionId = "1234567890";
+
+        // Identificador único de orden de compra: 26 caracteres maximo
+        $buyOrder = rand;
+
+        // URL Callback
+        $returnUrl = "https://dxs.cloud/cart/return/";
+        $finalUrl = "https://dxs.cloud/cart/voucher/";
+
+        // Iniciamos la transacción
+        $initResult = $transaction->initTransaction(
+                $amount, $buyOrder, $sessionId, $returnUrl, $finalUrl);
+
+        // Retorno:
+        // URL Largo 256 maximo
+        $formAction = $initResult->url;
+        // Token largo 64 maximo
+        $tokenWs = $initResult->token;
+
+        $this->view->amount = $amount;
+        $this->view->buyOrder = $buyOrder;
+        $this->view->formAction = $formAction;
+        $this->view->tokenWs = $tokenWs;
 
     }
 
