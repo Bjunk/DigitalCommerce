d51806013f1be1b7552aee2cfd6b906012d04497
Bjunk <pablo.aravenam@gmail.com>
2021-01-30 14:10:08 -0300

    mi cuenta

---
+++ app/controllers/MicuentaController.php
@@ -0,0 +1,25 @@
+<?php
+
+// Index Controller
+// 
+// Portalhaus SpA 2020
+// Versión 1.0
+// 
+// Controlador que maneja las interacciones de la página central
+//
+
+class MicuentaController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+    public function mispedidosAction()
+    {
+
+    }
+
+
+}
