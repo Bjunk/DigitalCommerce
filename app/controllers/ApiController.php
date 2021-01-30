73b6f8f423012c6956f5f714e2f592c7dfc1444b
Bjunk <pablo.aravenam@gmail.com>
2021-01-30 15:55:10 -0300

    api

---
+++ app/controllers/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+// Index Controller
+// 
+// Portalhaus SpA 2020
+// Versión 1.0
+// 
+// Controlador que maneja las interacciones de la página central
+//
+
+class ApiController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+	$this->view->disable();
+
+    }
+
+
+}
